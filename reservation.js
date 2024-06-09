07ad90640a7b132243c54438179074df6bd1d2ed
yasminabab <yasminabab9@gmail.con>
2024-06-09 12:30:00 -0400

    changes

--- reservation.js
+++ reservation.js
@@ -1,40 +1,66 @@
-const form = document.getElementById('reservation-form');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const email = document.getElementById('email');
+document.addEventListener('DOMContentLoaded', function() {
+    const serviceSelect = document.getElementById('serviceSelect');
+    const dropdownNumber = document.getElementById('dropdownNumber');
+    const expertSelect = document.getElementById('expertSelect');
+  
+    serviceSelect.addEventListener('change', function() {
+      if (serviceSelect.value === 'Cours en Groupe') {
+        dropdownNumber.style.display = 'block';
+      } else {
+        dropdownNumber.style.display = 'none';
+      }
 
-form.addEventListener('submit', (event) => {
-    validateForm();
-    if (!isFormValid()) {
-        event.preventDefault();
-    } else {
-        window.location.href = "confirmation.html"
-    }
+      const selectedService = serviceSelect.value;
+      let selectedExperts; 
+
+      switch (selectedService) {
+        case 'Cours en Groupe':
+            selectedExperts = ['Marie Dubois', 'Damien Martin', 'Noah Williams',
+                'Jessica Smith', 'Teresa Ramirez', 'Alejandro Garcia', 'Elke Schneider',
+                'Hans Neumann', 'Tao Zhang', 'Wei Lin']; 
+            break;
+        case 'Cours Particuliers':
+            selectedExperts = ['Marie Dubois', 'Damien Martin', 'Noah Williams',
+                'Jessica Smith', 'Teresa Ramirez', 'Alejandro Garcia', 'Elke Schneider',
+                'Hans Neumann', 'Tao Zhang', 'Wei Lin']; 
+            break;     
+        case 'Cours en ligne':
+            selectedExperts = ['Marie Dubois', 'Damien Martin', 'Noah Williams',
+                'Jessica Smith', 'Teresa Ramirez', 'Alejandro Garcia', 'Elke Schneider',
+                'Hans Neumann', 'Tao Zhang', 'Wei Lin']; 
+            break;  
+        case 'Cours en Réalité Virtuelle':
+            selectedExperts = ['Marie Dubois', 'Damien Martin', 'Noah Williams',
+                'Jessica Smith', 'Teresa Ramirez', 'Alejandro Garcia', 'Elke Schneider',
+                'Hans Neumann', 'Tao Zhang', 'Wei Lin'];             
+            break;  
+        case 'Préparation aux Examens':
+            selectedExperts = ['Damien Martin', 'Jessica Smith', 'Alejandro Garcia', 
+                'Hans Neumann', 'Wei Lin']; 
+            break;  
+        case 'Consultation en Orientation':
+            selectedExperts = ['Marie Dubois', 'Noah Williams', 'Teresa Ramirez', 
+                'Elke Schneider', 'Tao Zhang'];             
+            break;  
+        case 'Traduction':
+            selectedExperts = ['Eliza Robinson']; 
+            break;  
+        case 'Interprétation':
+            selectedExperts = ['Liam Jackson']; 
+            break;  
+                        
+        default:
+            selectedExperts = [];
+      }
+
+      expertSelect.innerHTML = '';
+
+    selectedExperts.forEach(function(expertName) {
+        const option = document.createElement('option');
+        option.text = expertName;
+        expertSelect.add(option);
+    });
+  });
 });
-
-function isFormValid() {
-    const firstNameValue = firstName.value.trim();
-    const lastNameValue = lastName.value.trim();
-    const emailValue = email.value.trim(); 
-
-    return (firstNameValue !== '' && lastNameValue !== '' && emailValue !== '');
-}
-
-function validateForm() {
-
-    const firstNameValue = firstName.value.trim();
-    const lastNameValue = lastName.value.trim();
-    const emailValue = email.value.trim();
-
-    if (firstNameValue === '') {
-        alert("Veuillez saisir votre prénom.");
-    }
-
-    if (lastNameValue === '') {
-        alert("Veuillez saisir votre nom.");
-    }
-
-    if (emailValue === '') {
-        alert("Veuillez saisir votre adresse mail.");
-    }
-}
+  
+  
